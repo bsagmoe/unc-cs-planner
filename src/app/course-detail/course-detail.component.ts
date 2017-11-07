809868f84040403d26c0ad6fefd12a92d02126f1
Ben <bsagmoe@gmail.com>
2017-11-06 23:02:29 -0500

    Update course detail component

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,30 +1,30 @@
 import { Input, Component, OnInit, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'
 import 'rxjs/add/operator/switchMap'
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database'
 
 import { CourseService } from '../services/course.service'
 import { PostService } from '../services/post.service'
 import { AuthService } from '../services/auth.service'
 import { UserService } from '../services/user.service'
 
+
 import { Course } from '../course'
 import { Post } from '../post'
-
-export type PlannerStatus =
-  'past'
-  | 'current'
-  | 'future'
-  | 'none'
+import { User } from '../user'
+import { UserCourse, PlannerStatus } from '../user-course'
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css'],
-  providers: [ CourseService, UserService ]
+  providers: [ CourseService, UserService, AuthService ]
 })
 
 export class CourseDetailComponent implements OnInit {
 
+  userCourses: UserCourse[];
+  courseKey: string;
   course: Course;           // The course itself
   posts: Post[];            // Relevant posts
   searchFinished: boolean;  // Used for displaying a progress bar
@@ -34,26 +34,30 @@ export class CourseDetailComponent implements OnInit {
   isFutureCourse: boolean;
 
   constructor(private postService: PostService,               // Gets the relevant posts
-              private authService: AuthService,
+              public authService: AuthService,
               private userService: UserService,               // Gets/updates the user's information
               private courseService: CourseService,           // Loads the course details
               private route: ActivatedRoute ) { }
 
   ngOnInit() {
     this.searchFinished = false;
+    this.isPastCourse = false;
+    this.isCurrentCourse = false;
+    this.isFutureCourse = false;
 
     // Dynamically load content depending on what the current URL is using switchMap
     this.route.params.switchMap((params: Params) => {
       if (params['modifier']) {
         // If there is a modifier such as 'H' for honors or 'L' for lab, react appropriately
-        return this.courseService.getCourseWithModifier(String(params['dept']), Number(params['number']), String(params['modifier']));
+        return this.courseService.getCourseWithModifier(String(params['dept']), Number(params['number']), String(params['modifier']))
+          .valueChanges();
       } else {
         // It's the 'main' course for the given number
-        return this.courseService.getCourse(String(params['dept']), Number(params['number']))
+        return this.courseService.getCourse(String(params['dept']), Number(params['number'])).valueChanges()
       }
     }).subscribe( course => {
       // The current course of the CourseDetailComponent is the result
-      this.course = course;
+      this.course = course as Course;
 
       // Sort the offerings in reverse chronological order!
       this.course.offerings.sort((a, b) => {
@@ -75,39 +79,31 @@ export class CourseDetailComponent implements OnInit {
       // Used for displayig a progress bar
       this.searchFinished = true;
 
+      this.postService.getPostsWithTag(this.course.dept + this.course.number).valueChanges()
+        .subscribe(posts => this.posts = posts.reverse());
+
+
       // See if the user has added the course to their planner anywhere
-      // TODO: update this to work with AuthService (probably by adding methods in the UserService to get the authenticated users info)
-      // let user = this.userContextService.getCurrentUser();
+      if (this.authService.authenticated) {
+        this.authService.afAuth.authState.subscribe(user => {
+          this.userService.getUserCourses(user.uid).valueChanges().subscribe(userCourses => {
+            this.userCourses = userCourses as UserCourse[];
 
-      // this.isPastCourse = false;
-      // this.isCurrentCourse = false;
-      // this.isFutureCourse = false;
+            this.userCourses.forEach(userCourse => {
+              localStorage.setItem(userCourse.course.dept + userCourse.course.number + userCourse.course.modifier,
+                                     userCourse.plannerStatus);
+            })
 
-      // if(user){
-      //   user.courses.past.forEach(course => {
-      //     if(course._id === this.course._id){
-      //       this.isPastCourse = true;
-      //     }
-      //   });
-
-      //   user.courses.current.forEach(course => {
-      //     if(course._id === this.course._id){
-      //       this.isCurrentCourse = true;
-      //     }
-      //   });
-
-      //   user.courses.future.forEach(course => {
-      //     if(course._id === this.course._id){
-      //       this.isFutureCourse = true;
-      //     }
-      //   });
-      // }
-
-      // Get the posts relevant to the current course
-      this.postService.getPostsWithTags([this.course.dept + ' ' + this.course.number ])
-        .then(posts => {
-          this.posts = posts;
-      }).catch()
+            this.userCourses.forEach(userCourse => {
+              if (userCourse.course.dept === this.course.dept && userCourse.course.number === this.course.number) {
+                this.isPastCourse = userCourse.plannerStatus === 'past';
+                this.isCurrentCourse = userCourse.plannerStatus === 'current';
+                this.isFutureCourse = userCourse.plannerStatus === 'future';
+              }
+            });
+          });
+        });
+      }
     })
 
   }
@@ -118,35 +114,17 @@ export class CourseDetailComponent implements OnInit {
     // If the button being clicked is the same one as is already active, we should remove the course from the planner
     status = shouldToggle ? 'none' : status
 
-    // this.userService.updateUserCourses(this.course._id, status)
-    //   .then(res => {
-
-    //     switch (status) {
-    //       case 'past':
-    //         this.isPastCourse = !this.isPastCourse;
-    //         this.isCurrentCourse = false;
-    //         this.isFutureCourse = false;
-    //         break;
-    //       case 'current':
-    //         this.isPastCourse = false;
-    //         this.isCurrentCourse = !this.isCurrentCourse;
-    //         this.isFutureCourse = false;
-    //         break;
-    //       case 'future':
-    //         this.isPastCourse = false;
-    //         this.isCurrentCourse = false;
-    //         this.isFutureCourse = !this.isFutureCourse;
-    //         break;
-    //       default:
-    //         this.isPastCourse = false;
-    //         this.isCurrentCourse = false;
-    //         this.isFutureCourse = false;
-    //         break;
-    //     }
-
-    //     // this.userContextService.setCurrentUser(res);
-    //   })
-    //   .catch();
+    if (status === 'none') {
+      this.userService.removeUserCourse(this.authService.uid, this.course).then( _ => {
+        this.isPastCourse = false;
+        this.isCurrentCourse = false;
+        this.isFutureCourse = false;
+      });
+      localStorage.removeItem(this.course.dept + this.course.number + this.course.modifier);
+    } else {
+      this.userService.updateUserCourses(this.authService.uid, this.course, status, this.courseKey);
+      localStorage.setItem(this.course.dept + this.course.number + this.course.modifier, status);
+    }
   }
 
 }
