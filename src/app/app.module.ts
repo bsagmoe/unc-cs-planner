4e1c37c764952a2bc0053701f779178085d1ba96
Ben <bsagmoe@gmail.com>
2017-11-06 23:16:15 -0500

    Update package.json and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material'
+
+import { MatTabsModule } from '@angular/material'
+import { MatIconModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+
 
 // Firebase
-import { AngularFireModule } from 'angularfire2'
-import { AngularFireAuthModule } from 'angularfire2/auth'
-import { AngularFireDatabaseModule } from 'angularfire2/database'
-import { environment } from 'environments/environment'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from 'environments/environment';
 
 // UNC CS Planner Services
-import { AuthService } from './services/auth.service'
-import { PostService } from './services/post.service'
-import { CommentService } from './services/comment.service'
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { CommentService } from './services/comment.service';
 
 // Components
 import { AppComponent } from './app.component';
@@ -31,7 +38,10 @@ import { OrdinalPipe } from './ordinal.pipe';
 import { LoginComponent } from './login/login.component';
 import { PostComponent } from './post/post.component';
 import { CommentComponent } from './comment/comment.component';
+import { ConfirmDeleteDialogComponent } from './dialogs/confirm-delete.component'
+import { VerifyEmailDialogComponent } from './dialogs/verify-email.component'
 import { PostPagerComponent } from './post-pager/post-pager.component';
+import { ParagraphsPipe } from './paragraphs.pipe';
 
 @NgModule({
   declarations: [
@@ -48,19 +58,27 @@ import { PostPagerComponent } from './post-pager/post-pager.component';
     PostComponent,
     CommentComponent,
     PostPagerComponent,
+    ConfirmDeleteDialogComponent,
+    VerifyEmailDialogComponent,
+    ParagraphsPipe
   ],
   imports: [
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
+    AngularFirestoreModule,
     AngularFireDatabaseModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatDialogModule,
+    // MatGridListModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
   ],
-  providers: [ PostService, AuthService ],
+  entryComponents: [ConfirmDeleteDialogComponent, VerifyEmailDialogComponent],
+  providers: [ PostService, AuthService, UserService ],
   bootstrap: [ AppComponent ]
 })
 
