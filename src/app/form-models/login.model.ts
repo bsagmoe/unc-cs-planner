b521b66d2e86f8c51c92df2a55cf4f7873d0894e
Ben <bsagmoe@gmail.com>
2017-11-06 23:05:23 -0500

    Add a new login model for the login form

--- src/app/form-models/login.model.ts
+++ src/app/form-models/login.model.ts
@@ -1,3 +1,18 @@
 export class LoginModel {
-  constructor(public email: string, public password: string){ }
+  constructor(
+    public email: string,
+    public password: string,
+  ) {};
+}
+
+export class RegistrationModel {
+  constructor(
+    public email: string,
+    public password: string,
+    public confirmPassword: string,
+    public firstName: string = null,
+    public lastName: string = null,
+    public graduationYear: number = null,
+    public isPrivate: boolean = false
+  ) { }
 }
